f84552c2a35f9b1d9707ae02f7f1ab4b46617e16
pandiyan <mycloudlms@outlook.com>
2020-10-14 22:15:44 +0530

    hi

---
+++ demo-1/instance.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "web-server" {
+  ami           = "${var.AMIS["${var.AWS_REGION}"]}"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "mycloudlms"
+  }
+}
---
+++ demo-1/vars.tf
@@ -0,0 +1,17 @@
+variable "AWS_ACCESS_KEY" {}
+
+variable "AWS_SECRET_KEY" {}
+
+variable "AWS_REGION" {
+  default = "eu-west-1"
+}
+
+variable "AMIS" {
+  type = map(string)
+  default = {
+    eu-west-1 = "ami-0bb3fad3c0286ebd5"
+    us-east-1 = "ami-yyy"
+    us-west-2 = "ami-zzz"
+
+  }
+}
