266368ee04b9c8bc421745097c32262ea32184da
pandiyan <mycloudlms@outlook.com>
2020-10-15 18:11:29 +0530

    demo-1 instance.tf update

--- demo-1/instance.tf
+++ demo-1/instance.tf
@@ -1,5 +1,6 @@
 resource "aws_instance" "web-server" {
-  ami           = "${var.AMIS["${var.AWS_REGION}"]}"
+#  ami           = "${var.AMIS["${var.AWS_REGION}"]}"
+   ami           = "${lookup(var.AMIS, var.AWS_REGION)}"
   instance_type = "t2.micro"
 
   tags = {
